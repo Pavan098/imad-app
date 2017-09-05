6e800ec50c83e1144c06dd50ba4a3b748c7f7d93
Pavan098 <motwanipavan19@gmail.com>
2017-09-05 12:02:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,10 +19,24 @@ button.onclick =function(){
     }  
       //not done yet
   };
-  
+  ,
   //make the request
   request.open('GET','http://motwanipavan19.imad.hasura-app.io/counter',true);
   request.send(null);
   
 
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+   //make a request to server and send the name
+   //capture a list of names and render it as a list
+   var names = ['name1','name2','name3','name4'];
+   var list ='';
+   for (var=0; i<names.length;i++){
+       list+= '<li>' + names[i] + '</li>';
+   }
+   var ul= document.getElementById('namelist');
+   ul.innerHTMl =list;
+} ;
