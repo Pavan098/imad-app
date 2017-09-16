d0683fc4925303fa6154cd9f342e10dbd2d6a91c
Pavan098 <motwanipavan19@gmail.com>
2017-09-16 23:32:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ submit.onclick = function() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   console.log(username);
-  consoloe.log(password);
+  consolo.log(password);
   var name = nameInput.value;
   request.open('POST','http://motwanipavan19.imad.hasura-app.io/login',true);
   request.setRequestHeader('Content-Type', 'application/json');
